96fa12aa536b0f3a4ef3eaebd5d67d2d730e1255
master-alen <nchufujianjian@126.com>
2017-10-17 21:27:19 +0800

    markDown 基础语法检入

--- blog-web/src/main/java/com/nchu/blog/BlogWebApplication.java
+++ blog-web/src/main/java/com/nchu/blog/BlogWebApplication.java
@@ -2,11 +2,19 @@ package com.nchu.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BlogWebApplication {
+public class BlogWebApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogWebApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BlogWebApplication.class);
+	}
+
 }
---
+++ blog-web/src/main/java/com/nchu/blog/action/IndexAction.java
@@ -0,0 +1,19 @@
+package com.nchu.blog.action;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * 主控制器
+ * @author fujianjian
+ * @project blog
+ * @date 2017/10/17 20:45
+ */
+@Controller
+public class IndexAction {
+
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
+}
